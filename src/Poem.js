a65a0c785596e58101917c06f9d3794eee0808ec
mav-code <mavisansmembres@gmail.com>
2020-04-29 10:54:33 -0400

    toggle read

--- src/Poem.js
+++ src/Poem.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 class Poem extends React.Component {
+  state = {
+    read: false
+  }
+
+  toggleRead = () => {
+    this.setState({
+      read: !this.state.read
+    })
+    console.log(this.state)
+  }
+
   render() {
     console.log(this.props.poem)
+    let button
+    if(this.state.read){
+      button = <button onClick={this.toggleRead}>Mark as unread</button>
+    } else {
+      button = <button onClick={this.toggleRead}>Mark as read</button>
+    }
     return (
       <div>
         <h3>{this.props.poem.title}</h3>
@@ -10,7 +27,7 @@ class Poem extends React.Component {
         <p>
           <strong>- By {this.props.poem.author}</strong>
         </p>
-        <button>Mark as read</button>
+        {button}
       </div>
     );
   }

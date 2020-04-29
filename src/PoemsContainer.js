d30368b9e6b0438d7758a3cd6b73a001b709e4da
mav-code <mavisansmembres@gmail.com>
2020-04-29 11:08:07 -0400

    favorites button

--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -3,32 +3,35 @@ import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
 
-  // state = {
-  //   poems: []
-  // }
+  state = {
+    favorites: [],
+    favoritesview: false
+  }
 
 
-  //fetch
-
-  // componentDidMount() {
-  //   fetch("http://localhost:6001/poems")
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         this.setState({
-  //           poems: result
-  //         })
-  //       }
-  //     )
-  // }
+toggleFavoriteView = () => {
+  this.setState({
+    favoritesview: !this.state.favoritesview
+  })
+}
 
 
   render() {
     console.log(this.props.poems)
+    let button
+    let poems
+    if(this.state.favoritesview){
+      button = <button onClick={this.toggleFavoriteView}>View All Poems</button>
+      poems = this.state.favorites
+    } else {
+      button = <button onClick={this.toggleFavoriteView}>View Favorite Poems</button>
+      poems = this.props.poems
+    }
     return (
       <div className="poems-container">
+      {button}
         {
-          this.props.poems.map(poem => <Poem poem={poem} key={poem.content}/>)
+          poems.map(poem => <Poem poem={poem} key={poem.content}/>)
         }
       </div>
     );
